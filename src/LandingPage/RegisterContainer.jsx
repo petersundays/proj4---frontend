fd8d1bc9ca588d7f510b7bc9f36a707128e833f1
Pedro Domingos <domingos@student.dei.uc.pt>
2024-03-09 18:03:24 +0000

    pagina login e registo aparecem e muda de uma para a outra

---
+++ src/LandingPage/RegisterContainer.jsx
@@ -0,0 +1,43 @@
+import './RegisterContainer.css';
+import './LandingPage.css';
+import React, { useState } from 'react';
+import LandingPage from './LandingPage';
+
+function registerContainer() {
+
+    const [showLandingPage, setShowLandingPage] = useState(false);
+
+    if (showLandingPage) {
+        return <LandingPage />;
+    }
+
+
+  return (
+    <div className="center-container-login">
+        <div className="registerPanel">
+            <img id="logo-register" src="src\multimedia\logo-scrum-01.png" width="250"/>
+            <form id="registrationForm" className="inputs-register">
+                <div className="right-inputs">
+                    <input type="text" className="inputRegister-fields" id="username-register" name="username" placeholder="Username" required/>
+                    <input type="password" className="inputRegister-fields" id="password-register" name="password" placeholder="Password" required/>
+                    <input type="password" className="inputRegister-fields" id="passwordConfirm-register" name="passwordConfirm" placeholder="Confirm Password" required/>
+                    <input type="email" className="inputRegister-fields" id="email-register" name="email" placeholder="Email" required/>
+                </div>
+                <div className="left-inputs">
+                    <input type="text" className="inputRegister-fields" id="firstName-register" name="firstName" placeholder="First Name" required/>
+                    <input type="text" className="inputRegister-fields" id="lastName-register" name="lastName" placeholder="Last Name" required/>
+                    <input type="text" className="inputRegister-fields" id="phone-register" name="phone" placeholder="Phone" required/>
+                    <input type="url" className="inputRegister-fields" id="photoURL-register" name="photoURL" placeholder="Photo URL" required/>
+                </div>
+                <div className="submitButton">
+                    <button type="submit" id="registerButton-register">Register</button>
+                </div>
+            </form>
+            <a id="backToLoginLink" onClick={() => setShowLandingPage(true)}>Login</a>
+            <p id="warningMessage4"></p>
+        </div>
+    </div>
+    );
+}
+
+export default registerContainer;
