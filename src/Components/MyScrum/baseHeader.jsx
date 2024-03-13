e664f5d09ccfda232ce3559f1d6612f0f4e56479
Pedro Domingos <domingos@student.dei.uc.pt>
2024-03-13 11:58:36 +0000

    footer, name and profile-pic

--- src/Components/MyScrum/baseHeader.jsx
+++ src/Components/MyScrum/baseHeader.jsx
@@ -1,6 +1,28 @@
+import React from 'react';
 import './baseHeader.css';
+import { UserStore } from '../../Stores/UserStore';
+
 
 function BaseHeader() {
+
+    console.log("UserStore: ", UserStore.getState().user);
+    let firstName = "";
+    if (UserStore.getState().user.firstName !== undefined) {
+        firstName = UserStore.getState().user.firstName;
+    } else {
+        firstName = "First Name";
+    }
+
+    let photoURL = "";
+    if (UserStore.getState().user.photoURL !== undefined) {
+        console.log("PHOTO: ", UserStore.getState().user.photoURL);
+        photoURL = UserStore.getState().user.photoURL;
+    } else {
+        console.log("No photo url");
+        photoURL = "multimedia/user-avatar.jpg";
+    }
+
+
     return (
         <>
             <header>
@@ -12,8 +34,8 @@ function BaseHeader() {
                     </ul>
                 </nav>
                 <div className="nav-menu-right">
-                    <img src="multimedia\user-avatar.jpg" id="profile-pic" draggable="false"/>
-                    <a href="edit-profile.html" id="first-name-label" draggable="false">FirstName</a>
+                    <img src={photoURL} id="profile-pic" draggable="false"/>
+                    <a href="edit-profile.html" id="first-name-label" draggable="false">{firstName}</a>
                     <button className="logout-button" id="logout-button-header">
                         <img src="multimedia/logout.png" alt="Logout Icon" draggable="false"/>
                         Logout
