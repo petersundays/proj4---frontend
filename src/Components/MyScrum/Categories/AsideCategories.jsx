a9d782b5fe11047f84c6be12a46eefb9462842af
Pedro Domingos <domingos@student.dei.uc.pt>
2024-03-17 02:46:22 +0000

    Categorias concluido Apenas falta apresentar tasks por categoria

--- src/Components/MyScrum/Categories/AsideCategories.jsx
+++ src/Components/MyScrum/Categories/AsideCategories.jsx
@@ -1,72 +1,242 @@
-import '../../General/Asides.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../../General/Button.jsx';
 import { UserStore } from '../../../Stores/UserStore.jsx';
 import { showErrorMessage } from '../../../functions/Messages/ErrorMessage';
 import { showSuccessMessage } from '../../../functions/Messages/SuccessMessage'; 
+import { CategoriesStore } from '../../../Stores/CategoriesStore.jsx';
 
 function AsideCategories() {
-
+    const token = UserStore.getState().user.token;
     const [newCategory, setNewCategory] = useState('');
+    const [categorySearch, setCategorySearch] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
+    const [categoriesLoaded, setCategoriesLoaded] = useState(false); 
+    const [displayCategoryModal, setDisplayCategoryModal] = useState(false);
+    const [newName, setNewName] = useState('');
+
+    useEffect(() => {
+        if (!categoriesLoaded) {
+            getCategoriesNames();  
+        }
+    }, [categoriesLoaded]); 
 
     const handleNewCategory = (e) => {
         setNewCategory(e.target.value);
     }
 
-    const createCategory = async (e) => {
+    const handleCategorySearch = (e) => {
+        setCategorySearch(e.target.value);
+    }
 
-        const token = UserStore.getState().user.token;
+    const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value);
+    }
+
+    const handleCategoryModal = () => {
+        if (selectedCategory === '') {
+            showErrorMessage('Please select a category to edit.');
+        } else {
+            console.log('selectedCategory', selectedCategory);
+            setDisplayCategoryModal(!displayCategoryModal);
+        }
+    }; 
+
+    const handleNewName = (e) => {
+        setNewName(e.target.value);
+    }
+
+
+    const getCategoriesNames = async () => {
+        const categories = await getCategories();
+        setCategories(categories);
+        setCategoriesLoaded(true); 
+    }
+
+    const createSelectOptions = () => {
+
+        if (categorySearch === '') {
+            return categories.map(category => (
+                <option key={category} value={category}>{category}</option>
+            ));
+        } else {
+            return categories.filter(category => category.toLowerCase().includes(categorySearch.toLowerCase())).map(category => (
+                <option key={category} value={category}>{category}</option>
+            ));
+        }
+    }
+
+    const handleDeleteCategory = () => {
+        if (selectedCategory === '') {
+            showErrorMessage('Please select a category to delete.');
+        } else {
+            deleteCategory();
+        }
+    }
+
+    const handleCategoryEdition = async () => {
+
+        const editCategory = `http://localhost:8080/backend_proj4_war_exploded/rest/users/editCategory/${selectedCategory}`;
+        try {
+            const response = await fetch(editCategory, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    token: token,
+                    newCategoryName: newName
+                },
+            });
+
+            if (response.ok) {
+                showSuccessMessage("Category edited successfully: " + newName);
+                setCategories(prevCategories => prevCategories.map(cat => cat === selectedCategory ? newName : cat));
+                setSelectedCategory(newName);
+                setDisplayCategoryModal(false);
+            } else {
+                const error = await response.text();
+                showErrorMessage('Error: ' + error);
+            }
+        }
+        catch (error) {
+            console.error('Error:', error);
+            showErrorMessage('Something went wrong. Please try again later.');
+        }
+    }     
+
+
+    const getCategories = async () => {
+        
+        const categoriesRequest = "http://localhost:8080/backend_proj4_war_exploded/rest/users/categories";
+        try {
+            const response = await fetch(categoriesRequest, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    token: token
+                }
+            });
+
+            if (response.ok) {
+                const categories = await response.json();
+                const categoriesNames = categories.map(category => category.name);
+                CategoriesStore.getState().setCategories(categoriesNames); 
+                return categoriesNames;
+            } else {
+                const error = await response.text();
+                showErrorMessage('Error: ' + error);
+                return [];
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            showErrorMessage('Something went wrong. Please try again later.');
+            return [];
+        }
+    }
+
+    const createCategory = async () => {
+
         const category = {
             name: newCategory
-          }
+        }
 
         const newCategoryUrl = "http://localhost:8080/backend_proj4_war_exploded/rest/users/newCategory";
 
         try {
-          const response = await fetch(newCategoryUrl, {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Accept': '*/*',
-                  token: token
-              },
-              body: JSON.stringify(category)
-          });
+            const response = await fetch(newCategoryUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    token: token
+                },
+                body: JSON.stringify(category)
+            });
 
-          if (response.ok) {
-              showSuccessMessage("'", category.name,"'", " category created successfully!");
-              
-          } else {
-              const error = await response.text();
-              showErrorMessage('Error: ' + error);
-          }
+            if (response.ok) {
+                showSuccessMessage("New category created successfully: " + category.name);
+                setCategories(prevCategories => [...prevCategories, newCategory]);
+                setNewCategory('');
+            } else {
+                const error = await response.text();
+                showErrorMessage('Error: ' + error);
+            }
         } catch (error) {
             console.error('Error:', error);
             showErrorMessage('Something went wrong. Please try again later.');
         }
     }
 
+    const deleteCategory = async () => {
+
+        const category = {
+            name: selectedCategory
+        }
+
+        const deleteCategoryUrl = `http://localhost:8080/backend_proj4_war_exploded/rest/users/deleteCategory/${category.name}`;
+
+        try {
+            const response = await fetch(deleteCategoryUrl, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                    token: token
+                },
+            });
+
+            if (response.ok) {
+                showSuccessMessage("Category deleted successfully: " + category.name);
+                setCategories(prevCategories => prevCategories.filter(cat => cat !== category.name));
+                setSelectedCategory('');
+            } else {
+                const error = await response.text();
+                showErrorMessage('Error: ' + error);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            showErrorMessage('Something went wrong. Please try again later.');
+        }
+    }
+
+    
 
     return ( 
-
         <>
             <aside>
                 <div className="add-task-container">
                     <h3 id="categories-h3">Categories</h3>
                     <label className="labels-search-category" id="label-category">Search</label>
-                    <input type="search" id="search-category" placeholder="Category"/>
-                    <select id="task-category" defaultValue={""} required/>
+                    <input type="search" id="search-category" placeholder="Category" onChange={handleCategorySearch}/>
+                    <select id="task-category" value={selectedCategory} onChange={handleCategoryChange} required>
+                        <option value="" disabled>Select category</option>
+                        {createSelectOptions()}
+                    </select>
                     <div id='category-buttons-container'>
-                        <Button text="Edit" width="120px" ></Button>
-                        <Button text="Search" width="120px" ></Button>
+                        <Button text="Edit" width="120px" onClick={handleCategoryModal}></Button>
+                        <Button text="Delete" width="120px" onClick={handleDeleteCategory} ></Button>
                     </div>
                     <div className='space-between'></div>
                     <label className="labels-create-category" id="label-category">New Category</label>
-                    <input type="text" id="create-category-name" placeholder="Category Name" onChange={handleNewCategory}/>
+                    <input type="text" id="create-category-name" placeholder="Category Name" value={newCategory} onChange={handleNewCategory}/>
                     <Button text="Create" onClick={createCategory}></Button>
                 </div>
+                <div id="passwordModal" className={`modal ${displayCategoryModal ? 'modalShown' : ''}`}>                
+                    <div className="modalContent">
+                        <label >Current Name</label>
+                        <input type="text" id="category-current-name" value={selectedCategory}  readOnly />
+                        <label >New Name</label>
+                        <input type="text" id="category-new-name" placeholder="New Name" onChange={handleNewName}  required />
+                        <div className="modal-buttons">
+                            <Button text="Cancel" onClick={handleCategoryModal}/>
+                            <Button type="submit" text="Save" onClick={handleCategoryEdition} />
+                        </div>
+                </div>
+            </div>
             </aside>
-    </>
-    )
+        </>
+    );
 }
+
 export default AsideCategories;
